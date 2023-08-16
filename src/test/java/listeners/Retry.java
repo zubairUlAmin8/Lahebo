6a0b8cf8eca394f31804b7548d65974840c1d088
Zubair Ul Amin <zubairulamin8@gmail.com>
2023-08-16 14:37:15 +0500

    all for all test cases

--- src/test/java/listeners/Retry.java
+++ src/test/java/listeners/Retry.java
@@ -5,7 +5,7 @@ import org.testng.ITestResult;
 
 public class Retry implements IRetryAnalyzer {
     private int count = 0;
-    private static int maxTry = Integer.parseInt("2");
+    private static int maxTry = Integer.parseInt("1");
 
     @Override
     public boolean retry(ITestResult iTestResult) {
